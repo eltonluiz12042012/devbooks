8e149fe401509bc8df43ac536aa61b41660f12fe
Elton Luiz <eltonluiz12042012@gmail.com>
2023-06-12 22:46:50 -0300

    Comunicação entre a pagina de cadastro e api

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -3,6 +3,7 @@ $('#formulario-cadastro').on('submit', criarUsuario);
 function criarUsuario(evento) {
     evento.preventDefault();
 
+
     if ($('#senha').val() != $('#confirmar-senha').val()) {
         alert("As senhas são diferentes!")
         return
